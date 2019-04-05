ec6c01493cacfd786e66ffdf5f312b7c8eb2f539
Victor Arza <papu.arza@gmail.com>
2019-04-05 09:12:25 -0400

    new

--- bin/retrieve-data.js
+++ bin/retrieve-data.js
@@ -3,6 +3,7 @@ const crypto = require("crypto");
 const mongoose = require('mongoose');
 const User = require('../models/User.js');
 const dbName = process.env.MONGODB_URI;
+const Code = require('../models/Code.js');
 const emailing = require('../nodemailer/config.js');
 
 mongoose.Promise = Promise;
@@ -11,9 +12,8 @@ mongoose.connect(dbName)
   .then(() => {
     User.findOne({email: 'me_schroder@hotmail.com'})
     .populate('codes')
-    .then(users => {
-      let sendEmails = [];
-        users.codes.forEach(code => {
+    .then(user => {
+        user.codes.forEach(code => {
             console.log(code)
         })
     })
